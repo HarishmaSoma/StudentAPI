e0167f0dd102cbaae59ec0da04e4bdcce87ebe42
HarishmaSoma <soma.harishma01@infosys.com>
2025-02-07 14:47:18 +0530

    context added

--- obj/Debug/net8.0/StudentAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e760c99d7c909acd49638fd81adf54273cf207ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c11b9f7cd2ac4ba3534ac4abf0d177d7bb5afeee")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
